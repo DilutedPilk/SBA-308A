c56deb840ac14a7f5cc34b4e0e4bcaff4af01fa4
dilutedpilk <myawesome4@gmail.com>
2026-06-10 19:58:15 -0400

    Created search inputs and functions which gets value of inputs

--- src/script.js
+++ src/script.js
@@ -1,3 +1,14 @@
 const countrySelect = document.getElementById('country')
 const citySelect = document.getElementById('city')
 const breweries = document.getElementById('brew_list')
+const brewSearch = document.getElementById('brew_search')
+
+function getCountry() {
+    const country = countrySelect.value;
+    return country;
+}
+
+function getCity() {
+    const city = citySelect.value;
+    return city;
+}
