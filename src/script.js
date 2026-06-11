dd18704077e534464b2c5f60015279645362e3d6
dilutedpilk <myawesome4@gmail.com>
2026-06-10 22:41:09 -0400

    When typing city names, page shows information about breweries in that particular city

--- src/script.js
+++ src/script.js
@@ -21,10 +21,37 @@ async function getBreweries(city, page) {
     }
 }
 
-brewSearch.addEventListener('click', (e) => {
+function displayBreweries(arr) {
+    console.log(arr)
+    for (let i = 0; i <arr.length; i++) {
+        const newSection = document.createElement("section")
+        newSection.classList.add("breweries");
+        const title = document.createElement('h3')
+        title.classList.add('title')
+        title.textContent = arr[i].name
+        const hr = document.createElement('hr')
+        const address = document.createElement('p')
+        address.textContent = `${arr[i].address_1}, ${arr[i].city} ${arr[i].postal_code}`
+        const phone = document.createElement('p')
+        phone.textContent = arr[i].phone
+        const link = document.createElement('a')
+        link.href = arr[i].website_url
+        link.textContent = "Website"
+        breweries.appendChild(newSection);
+        newSection.appendChild(title)
+        newSection.appendChild(hr)
+        newSection.appendChild(address)
+        newSection.appendChild(link)
+        newSection.appendChild(phone)
+    }
+}
+
+brewSearch.addEventListener('click', async (e) => {
     e.preventDefault();
     if (e.target == searchButton) {
         let city = getCity();
-        console.log(getBreweries(city, 10))
+        let breweriesArr = await getBreweries(city, 10);
+        breweries.innerHTML = ""
+        displayBreweries(breweriesArr);
     }
 })
