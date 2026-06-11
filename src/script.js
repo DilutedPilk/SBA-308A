482a64d55a055af1b276c2c70edf7987c9f2588b
dilutedpilk <myawesome4@gmail.com>
2026-06-10 21:06:25 -0400

    Event listener gets promise items

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ const citySelect = document.getElementById('city');
 const breweries = document.getElementById('brew_list');
 const brewSearch = document.getElementById('brew_search');
 const searchButton = document.getElementById('search');
-const URL = "https://api.openbrewerydb.org/v1/breweries/"
+const URL = "https://api.openbrewerydb.org/v1/breweries"
 
 function getCity() {
     const city = citySelect.value.toLowerCase();
@@ -11,12 +11,20 @@ function getCity() {
     return cityURL;
 }
 
-async function getBreweries(city) {
-    const breweries = await fetch()
+async function getBreweries(city, page) {
+    const response = await fetch(`${URL}?by_city=${city}&per_page=${page}`)
+    try {
+        const result = await response.json();
+        return result
+    } catch (error) {
+        console.error(error.message);
+    }
 }
 
 brewSearch.addEventListener('click', (e) => {
     e.preventDefault();
     if (e.target == searchButton) {
+        let city = getCity();
+        console.log(getBreweries(city, 10))
     }
 })
