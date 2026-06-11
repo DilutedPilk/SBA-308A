10b09a2cc1c2a299ad2e3ade9f761692eb229d66
dilutedpilk <myawesome4@gmail.com>
2026-06-10 20:32:13 -0400

    Function getCity was tested and works properly

--- src/script.js
+++ src/script.js
@@ -1,14 +1,22 @@
-const countrySelect = document.getElementById('country')
-const citySelect = document.getElementById('city')
-const breweries = document.getElementById('brew_list')
-const brewSearch = document.getElementById('brew_search')
-
-function getCountry() {
-    const country = countrySelect.value;
-    return country;
-}
+const citySelect = document.getElementById('city');
+const breweries = document.getElementById('brew_list');
+const brewSearch = document.getElementById('brew_search');
+const searchButton = document.getElementById('search');
+const URL = "https://api.openbrewerydb.org/v1/breweries/"
 
 function getCity() {
-    const city = citySelect.value;
-    return city;
+    const city = citySelect.value.toLowerCase();
+    const cityARR = city.split(" ")
+    const cityURL = cityARR.join("_")
+    return cityURL;
 }
+
+async function getBreweries(city) {
+    const breweries = await fetch()
+}
+
+brewSearch.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (e.target == searchButton) {
+    }
+})
